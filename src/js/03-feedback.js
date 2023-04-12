5c6ce2a71c963f1571f6691463f7ca453914f7e9
PoliukhovychAnna <116736506+PoliukhovychAnna@users.noreply.github.com>
2023-04-12 21:33:21 +0300

    task-3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,29 +1,37 @@
-// import throttle from "lodash.throttle";
+import throttle from "lodash.throttle";
 
-// const form = document.querySelector('.feedback-form');
-// const email = document.querySelector(".email");
-// const message = document.querySelector(".message");
-// const STORAGE_KEY = 'feedback-form-state';
-// form.addEventListener("input", fillData)
-// // form.addEventListener("submit", onSubmit)
+const form = document.querySelector('.feedback-form');
+const STORAGE_KEY = 'feedback-form-state';
 
-// function fillData(e) {
+fillData()
 
-// //   localStorage.getItem(STORAGE_KEY) ?? "";
+form.addEventListener(
+  'input',throttle(() => saveData(), 500));
+form.addEventListener("submit", onSubmit)
 
-//       localStorage.setItem(
-//         STORAGE_KEY,
-//         JSON.stringify({ email: form.email.value, message: form.message.value })
-//       );
+function saveData() {
+    const userData = {
+      email: form.email.value.trim(),
+      message: form.message.value.trim(),
+    };
 
-//       if (STORAGE_KEY) {
-//           form.email.value = localStorage.getItem(STORAGE_KEY.email);
-//       }
+      localStorage.setItem(STORAGE_KEY, JSON.stringify(userData));
+}
 
-// }
+function fillData() {
+     const savedData = JSON.parse(localStorage.getItem(STORAGE_KEY)) ?? {};
+     form.email.value = savedData.email ?? '';
+     form.message.value = savedData.message ?? '';
+}
 
-// function onSubmit() {
-//     e.preventDefault();
-//       e.currentTarget.reset();
-//       localStorage.removeItem(STORAGE_KEY);
-//     }
+function onSubmit(e) {
+    e.preventDefault()
+    if (form.email.value.trim() === '' || form.message.value.trim() === '') {
+      return;
+    } else {
+        const savedData = JSON.parse(localStorage.getItem(STORAGE_KEY)) ?? {};
+        console.log(savedData);
+      localStorage.removeItem(STORAGE_KEY);
+      form.reset();
+    }
+}
